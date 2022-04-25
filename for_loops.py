9d12450db5bc79ecdd36bfbee4644990b1fcadc5
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2022-04-25 17:09:39 +0100

    Created my second program with python

---
+++ for_loops.py
@@ -0,0 +1,40 @@
+# for Loops
+
+# prices = [10, 20, 30, 40]
+# total = 0
+# for price in prices:
+#     total += price
+# print(f"Total: {total}")
+
+
+# Nested Loops
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+
+# numbers = [5, 2, 5, 2, 2]
+# for x_count in numbers:
+#     output = ""
+#     for count in range(x_count):
+#         output += "X"
+#     print(output)
+
+# numbers = [3, 6, 2, 8, 4, 10]
+# max = numbers[0]
+# for number in numbers:
+#     if number > max:
+#         max = number
+# print(max)
+
+# Two Dimensional List
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# for row in matrix:
+#     for items in row:
+#         print(items)
+
+
